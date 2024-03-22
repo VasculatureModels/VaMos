0e0cf9655de1d4a04048ec6c39fd1383d3bccc62
VasculatureModels <144987101+VasculatureModels@users.noreply.github.com>
2024-03-22 08:45:47 +0100

    Update GetGraph.py

--- MyFunctions/GetGraph.py
+++ MyFunctions/GetGraph.py
@@ -1,68 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Copyright - Florent Autrusseau
-contributor(s) : Florent Autrusseau, Rafic Nader (February 2023)
 
-Florent.Autrusseau@univ-nantes.fr
-Rafic.Nader@univ-nantes.fr
-
-This software is a computer program whose purpose is to detect cerebral
-vascular tree bifurcations within MRA-TOF acquisitions.
-
-This software is governed by the CeCILL  license under French law and
-abiding by the rules of distribution of free software.  You can  use,
-modify and/ or redistribute the software under the terms of the CeCILL
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info".
-
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited liability.
-
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or
-data to be ensured and,  more generally, to use and operate it in the
-same conditions as regards security.
-
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
-"""
-
-#import mayavi.mlab as mlab
-#from mayavi.mlab import points3d
-#from mayavi.mlab import plot3d
-#from affiche_point import affiche_point
-
-# Local imports:
 import sknw
-#import time
-#import cv2
-#import math
-#import argparse
-#import numpy as np
-#import pandas as pd
-#import skimage.util
-#from math import sqrt
-#from math import acos
-#import SimpleITK as sitk
-#from scipy import ndimage
-#from pathlib import Path
-#from decimal import Decimal
-#from pandas import ExcelWriter
-#import sys, os, glob, platform
-#from scipy.stats import entropy
-#import matplotlib.pyplot as plt
-#import skimage.io #algos de traitement d'images
-#import scipy.ndimage as ndi #pour la distance transform
+
 from skimage.morphology import skeletonize_3d
 
 from MyFunctions.cross_section_diameter2021 import *
