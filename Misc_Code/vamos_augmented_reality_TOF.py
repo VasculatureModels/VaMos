0b2d19d9f22d5b3815414a8c6a397162fd2ca138
VasculatureModels <144987101+VasculatureModels@users.noreply.github.com>
2024-03-22 09:15:52 +0100

    Update vamos_augmented_reality_TOF.py

--- Misc_Code/vamos_augmented_reality_TOF.py
+++ Misc_Code/vamos_augmented_reality_TOF.py
@@ -2,12 +2,12 @@
 import SimpleITK as sitk
 import numpy as np
 
-s_orig = sitk.ReadImage("/home/florent/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_Orig.nrrd")
-s_vasc = sitk.ReadImage("/home/florent/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_AGr=0.8_CleanVascu.seg.nrrd")
-s_segm = sitk.ReadImage("/home/florent/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_AGr=0.8_BinOrig.seg.nrrd")
-s_icasegm = sitk.ReadImage("/home/florent/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_ICA.seg.nrrd")
-s_vamos = sitk.ReadImage("/home/florent/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_SigSt=0.8_R=3_Sed=0.0_AGr=0.8_Model.nrrd")
-s_labels = sitk.ReadImage("/home/florent/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_AGr=0.8_ICA_Label_Model.seg.nrrd")
+s_orig = sitk.ReadImage("/home/---/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_Orig.nrrd")
+s_vasc = sitk.ReadImage("/home/---/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_AGr=0.8_CleanVascu.seg.nrrd")
+s_segm = sitk.ReadImage("/home/---/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_AGr=0.8_BinOrig.seg.nrrd")
+s_icasegm = sitk.ReadImage("/home/---/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_ICA.seg.nrrd")
+s_vamos = sitk.ReadImage("/home/---/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_SigSt=0.8_R=3_Sed=0.0_AGr=0.8_Model.nrrd")
+s_labels = sitk.ReadImage("/home/---/Images/MRA-Dataset/_ICA_Model/14_Bif=13_Spl=1_R=3_Sed=0.0_AGr=0.8_ICA_Label_Model.seg.nrrd")
 
 
 orig = sitk.GetArrayFromImage(s_orig)
@@ -22,4 +22,4 @@ augm_model[segm > 0] = vamos[segm > 0]
 augm_model = np.maximum(augm_model, vamos)
 augm_model[labels > 0] = vamos[labels > 0]
 
-sitk.WriteImage(sitk.GetImageFromArray(augm_model), "/home/florent/Bureau/AugmModel.nrrd")
+sitk.WriteImage(sitk.GetImageFromArray(augm_model), "/home/---/Bureau/AugmModel.nrrd")
