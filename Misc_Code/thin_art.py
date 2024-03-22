cba3659b48e7b80927e834ce74225efb5bc2fd55
VasculatureModels <144987101+VasculatureModels@users.noreply.github.com>
2024-03-22 09:15:37 +0100

    Update thin_art.py

--- Misc_Code/thin_art.py
+++ Misc_Code/thin_art.py
@@ -3,8 +3,6 @@ import numpy as np
 import SimpleITK as sitk
 
 labelsN = sys.argv[1]
-#labelsN = "/home/florent/Images/MRA-Dataset/59_labels.seg.nrrd"
-#labelsN2 = "/home/florent/Bureau/Arteries_Labels/59_labels.seg.nrrd"
 
 BaseName = (os.path.basename(labelsN))
 FileDir = os.path.dirname(os.path.abspath(labelsN)) + '/'
