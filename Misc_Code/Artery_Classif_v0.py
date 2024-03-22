f713f827b8aaa5f4ececfac19787d22eeac2952b
VasculatureModels <144987101+VasculatureModels@users.noreply.github.com>
2024-03-22 09:11:12 +0100

    Update Artery_Classif_v0.py

--- Misc_Code/Artery_Classif_v0.py
+++ Misc_Code/Artery_Classif_v0.py
@@ -1,40 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Copyright - Florent Autrusseau
-contributor(s) : Florent Autrusseau, Rafic Nader (February 2023)
-
-Florent.Autrusseau@univ-nantes.fr
-
-This software is a computer program whose purpose is to detect cerebral
-vascular tree bifurcations within MRA-TOF acquisitions.
-
-This software is governed by the CeCILL  license under French law and
-abiding by the rules of distribution of free software.  You can  use,
-modify and/ or redistribute the software under the terms of the CeCILL
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info".
-
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited liability.
-
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or
-data to be ensured and,  more generally, to use and operate it in the
-same conditions as regards security.
-
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
-"""
 
 #import napari
 import json
@@ -65,11 +31,11 @@ from MyFunctions.misc_fun import *
 ################################################################################################################################################
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str, default='/home/florent/Images/MRA-Dataset/129.nii',
+ap.add_argument("-i", "--image", type=str, default='/home/---/Images/MRA-Dataset/129.nii',
 	help="Input 3D image (stack) (.nrrd, .nii or .mha)")
-ap.add_argument("-seg", "--seg", type=str, default='/home/florent/Images/MRA-Dataset/129.seg.nii',
+ap.add_argument("-seg", "--seg", type=str, default='/home/---/Images/MRA-Dataset/129.seg.nii',
 	help="Segmented input 3D image (stack)")
-ap.add_argument("-j", "--json", type=str, default='/home/florent/Images/MRA-Dataset/129.seg_F.mrk.json',
+ap.add_argument("-j", "--json", type=str, default='/home/---/Images/MRA-Dataset/129.seg_F.mrk.json',
     help="path for json file which contains the ground truth  the bif positions")
 
 args = vars(ap.parse_args())
